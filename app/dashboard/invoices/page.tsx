e05bda6e7418890e07a8cc61bcf73bacd16bce47
victor grajales <vmgrajalesr@gmail.com>
2024-10-03 10:59:12 -0500

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+
+
+function page() {
+  return (
+    <h1>InVoices Page</h1>
+  )
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+function page() {
+  return (
+    <h1>Dashboard Page</h1>
+  )
+}
+
+export default page
